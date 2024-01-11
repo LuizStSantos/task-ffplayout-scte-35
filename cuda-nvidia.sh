92b1947a263f922111112a81c962bc50480d1359
LuizStSantos <73047610+LuizStSantos@users.noreply.github.com>
2024-01-11 18:01:54 -0300

    Update cuda-nvidia.sh

--- cuda-nvidia.sh
+++ cuda-nvidia.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Verifica se o script está sendo executado como root
-if [[ $EUID -ne 0 ]]; then
-    echo "Este script deve ser executado como root. Por favor, use sudo ou mude para o usuário root."
-    exit 1
-fi
-
 # Instalar pacotes necessários
 apt install git curl build-essential wget sudo -y
 
