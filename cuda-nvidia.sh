891554d89cbc6b73da57589f2de92a59a3824d50
LuizStSantos <73047610+LuizStSantos@users.noreply.github.com>
2024-01-11 16:17:17 -0300

    Update cuda-nvidia.sh

--- cuda-nvidia.sh
+++ cuda-nvidia.sh
@@ -4,7 +4,7 @@
 apt update && apt upgrade -y
 
 # Instalar pacotes necessários
-apt install git curl build-essential wget -y
+apt install git curl build-essential wget
 
 # Adicionar configurações ao arquivo de blacklist
 bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
