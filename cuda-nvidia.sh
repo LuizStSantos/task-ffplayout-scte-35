7e02bcdc2a11e80404760217add0672688e64665
LuizStSantos <73047610+LuizStSantos@users.noreply.github.com>
2024-01-11 18:03:00 -0300

    Update cuda-nvidia.sh

--- cuda-nvidia.sh
+++ cuda-nvidia.sh
@@ -3,16 +3,19 @@
 # Instalar pacotes necessários
 apt install git curl build-essential wget sudo -y
 
+
 # Bloqueia o driver nouveau
-echo "blacklist nouveau" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
-echo "options nouveau modeset=0" | sudo tee -a /etc/modprobe.d/blacklist-nouveau.conf
+cat <<EOF > /etc/modprobe.d/blacklist-nouveau.conf
+blacklist nouveau
+options nouveau modeset=0
+EOF
 
 # Atualizar initramfs
 update-initramfs -u
 
 # Baixar e instalar CUDA
 wget -c https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda_12.3.2_545.23.08_linux.run
-chmod 755 cuda_12.3.2_545.23.08_linux.run
+chmod +x cuda_12.3.2_545.23.08_linux.run
 ./cuda_12.3.2_545.23.08_linux.run --override --driver --toolkit --silent
 
 echo "Instalação concluída com sucesso."
