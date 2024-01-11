936cb3fcf956513fbe31d1cff86f9fab12333464
LuizStSantos <73047610+LuizStSantos@users.noreply.github.com>
2024-01-11 16:30:00 -0300

    Delete ffmpeg-python3-pip-x9k3.sh

--- ffmpeg-python3-pip-x9k3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update
-apt install ffmpeg
-apt install -y python3 python3-pip
-rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
-python3 -mpip install x9k3
-echo "Instalação do FFmpeg, Python 3, pip e x9k3 concluída com sucesso."
-echo "O diretório /usr/lib/python3.*/EXTERNALLY-MANAGED foi removido."
