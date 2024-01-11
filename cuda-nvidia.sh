996daefc1813d2b99f6c2a45f9b8526f60aafd92
LuizStSantos <73047610+LuizStSantos@users.noreply.github.com>
2024-01-11 17:25:34 -0300

    Update cuda-nvidia.sh

--- cuda-nvidia.sh
+++ cuda-nvidia.sh
@@ -6,12 +6,6 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# Adiciona o novo repositório
-echo "deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware" >> /etc/apt/sources.list
-
-# Atualizar o sistema
-apt update && apt upgrade -y
-
 # Instalar pacotes necessários
 apt install git curl build-essential wget sudo -y
 
@@ -21,12 +15,15 @@ blacklist nouveau
 options nouveau modeset=0
 EOF
 
+# Parar o serviço LightDM (substitua pelo comando correto se estiver usando outro gerenciador de exibição)
+service lightdm stop
+
 # Atualizar initramfs
-sudo update-initramfs -u
+update-initramfs -u
 
 # Baixar e instalar CUDA
 wget -c https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda_12.3.2_545.23.08_linux.run
-chmod +x cuda_12.3.2_545.23.08_linux.run
+chmod 755 cuda_12.3.2_545.23.08_linux.run
 ./cuda_12.3.2_545.23.08_linux.run --override --driver --toolkit --silent
 
 echo "Instalação concluída com sucesso."
