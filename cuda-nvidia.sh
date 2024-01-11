e6cf372d1532f8b040acdaf77673346c613b3472
LuizStSantos <73047610+LuizStSantos@users.noreply.github.com>
2024-01-11 15:12:37 -0300

    Update cuda-nvidia.sh

--- cuda-nvidia.sh
+++ cuda-nvidia.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-apt update
-apt install git curl build-essential wget sudo
+apt install git curl build-essential wget
 cat <<EOF > /etc/modprobe.d/blacklist-nouveau.conf
 blacklist nouveau
 options nouveau modeset=0
-EOF
 update-initramfs -u
 wget -c -P /tmp/ https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda_12.3.2_545.23.08_linux.run
 chmod +x /tmp/cuda_12.3.2_545.23.08_linux.run
