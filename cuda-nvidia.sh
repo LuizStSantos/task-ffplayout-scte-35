f53c56750a57cffadbd713ea38f4f069623990a0
LuizStSantos <73047610+LuizStSantos@users.noreply.github.com>
2024-01-11 22:05:57 -0300

    Update cuda-nvidia.sh

--- cuda-nvidia.sh
+++ cuda-nvidia.sh
@@ -15,9 +15,14 @@ update-initramfs -u
 # Stop the display manager (assuming lightdm, adjust if necessary)
 sudo service lightdm stop
 
-# Baixar e instalar CUDA
+# Baixar CUDA e dar permissão 
 wget -c https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda_12.3.2_545.23.08_linux.run
 chmod 755 cuda_12.3.2_545.23.08_linux.run
+
+# Instalar cabeçalhos do kernel
+apt install linux-headers-$(uname -r) -y
+
+# Executar o instalador do CUDA
 sh ./cuda_12.3.2_545.23.08_linux.run --override --driver --toolkit --silent
 
 echo "Instalação concluída com sucesso."
