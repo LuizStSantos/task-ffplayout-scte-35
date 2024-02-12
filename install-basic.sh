93bfd319d4a0e2b0594a0f3b10a5b3cc09546a5c
LuizStSantos <73047610+LuizStSantos@users.noreply.github.com>
2024-02-12 12:40:39 -0300

    Add files via upload

---
+++ install-basic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Atualizar o sistema
+sudo apt update
+
+# Instalar alguns softwares
+apt install -y ffmpeg ntfs-3g sudo
+
+
+echo "Foi instalado ffmpeg, ntfs-3g e sudo."
+echo "Instalação concluída com sucesso."
